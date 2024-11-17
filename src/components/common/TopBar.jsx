eeff3e8a7e6242fdd4cb58dae9822f259c1049ad
seunghyeonKang <number42isuser@gmail.com>
2024-11-17 18:11:32 +0900

    � Start: 임시 tob bar 추가

--- src/components/common/TopBar.jsx
+++ src/components/common/TopBar.jsx
@@ -1,14 +1,36 @@
+import { useNavigate } from "react-router-dom";
 import * as A from "./TopBar.style";
 
 export default function TopBar() {
+  const navigate = useNavigate();
+
+  const handleMoveToMain = () => {
+    navigate(`/`);
+  };
+  const handleMoveToSearch = () => {
+    navigate(`/search`);
+  };
+  const handleMoveToGuestBook = () => {
+    navigate(`/guest-book`);
+  };
+  const handleMoveToProjectList = () => {
+    //삭제예정
+    navigate(`/project-list`);
+  };
+  const handleMoveToProjectDetail = () => {
+    //삭제예정
+    navigate(`/project-detail`);
+  };
+
   return (
     <>
       <A.BarContainer>
-        DS Archive
-        {/* <button>검색</button>
-        <button>방명록</button>
-        <button>(임시)프로젝트 목록</button>
-        <button>(임시)프로젝트 상세</button> */}
+        top bar
+        <button onClick={handleMoveToMain}>DS Archive</button>
+        <button onClick={handleMoveToSearch}>검색</button>
+        <button onClick={handleMoveToGuestBook}>방명록</button>
+        <button onClick={handleMoveToProjectList}>(임시)프로젝트 목록</button>
+        <button onClick={handleMoveToProjectDetail}>(임시)프로젝트 상세</button>
       </A.BarContainer>
     </>
   );
--- src/components/common/TopBar.style.js
+++ src/components/common/TopBar.style.js
@@ -3,6 +3,8 @@ import color from "styles/color";
 import font from "styles/font";
 
 export const BarContainer = styled.div`
+  height: 5.8rem;
   ${font.regular_16}
   background-color: ${() => color.grayscale_ea};
 `;
+export const NavButton = styled.button``;
